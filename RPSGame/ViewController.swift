af457684cf78dc7ad686378de447ff65e3b4acff
ByeongHyeon <qudgus1984@naver.com>
2022-05-16 19:52:24 +0900

    아웃렛 변수 선언 및 액션 함수 연결 완료

--- RPSGame/ViewController.swift
+++ RPSGame/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 변수 / 속성
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    
+    
+    // 함수 / 메서드
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    
 
 }
 
